2da2fb375df0bb7f824915a3289d50434643c451
babixGO-GO <github@babixgo-mail.de>
2026-03-08 19:58:58 +0100

    Add TriggerAttemptRoll function for manual roll triggering

--- jni/hooks/roll_hook.h
+++ jni/hooks/roll_hook.h
@@ -4,5 +4,8 @@ namespace Hooks::Roll {
 
 bool Install();
 
-}
+// Manual roll trigger - calls AttemptRoll on the RollService instance
+// Returns true if successful, false if hook not installed or no instance available
+bool TriggerAttemptRoll();
 
+}
