7b7a2ec05b30ce960fe8f7343620758110c88edc
babixGO-GO <github@babixgo-mail.de>
2026-03-08 19:41:39 +0100

    Add speed multiplier control to speed hook

--- jni/hooks/speed_hook.h
+++ jni/hooks/speed_hook.h
@@ -4,5 +4,10 @@ namespace Hooks::Speed {
 
 bool Install();
 
-}
+// Speed multiplier control
+void SetSpeedMultiplier(float multiplier);
+float GetSpeedMultiplier();
+void EnableSpeedModification(bool enabled);
+bool IsSpeedModificationEnabled();
 
+}
